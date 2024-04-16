60b64a2c6b19e46f015bc26f80e3b8573dae93ea
Zenith Zack <faldund@outlook.com>
2024-04-16 16:22:22 -0400

    feat(unittest): fail a unittest

--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -6,22 +6,21 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UnitTest1
 {
-	TEST_CLASS(UnitTest1)
-	{
-	public:
-		
-		TEST_METHOD(TestMethod1)
-		{
+	TEST_CLASS(UnitTest1){
+		public :
 
-			CoolClass coolClass;
-			Assert::AreEqual(3, coolClass.Calc(1, 2));
-		}
+			TEST_METHOD(TestMethod1){
 
-		TEST_METHOD(TestMethod2)
-		{
+				CoolClass coolClass;
+	Assert::AreEqual(3, coolClass.Calc(1, 2));
+}
 
-			CoolClass coolClass;
-			Assert::AreEqual(5, coolClass.Calc(3, 2));
-		}
-	};
+TEST_METHOD(TestMethod2)
+{
+
+	CoolClass coolClass;
+	Assert::AreEqual(6, coolClass.Calc(3, 2));
+}
+}
+;
 }
